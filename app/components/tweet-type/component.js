0d6d7bf2fab83b8d42dba1a625886b3ff3f6d8d0
zinyando <brizdigital@gmail.com>
2015-06-18 12:38:45 +0200

    Add active toggle state to categories

--- app/components/tweet-type/component.js
+++ app/components/tweet-type/component.js
@@ -1,6 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  didInsertElement: function(){
+    this.$(".type").click(function(){
+      $(".type").removeClass("active");
+      $(this).addClass("active");
+    });
+  },
+
   actions: {
     submit(category){
       this.sendAction('action', category);
