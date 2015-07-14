e584f494792044dd7c35fdfbbaa73f1bddaa549f
Taras Mankovski <tarasm@gmail.com>
2015-07-14 09:51:12 -0400

    Refactored to use the forked version of dependencies

--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -1,24 +1,6 @@
 import Ember from 'ember';
+import ToriiFirebaseAdapter from 'emberfire/torii-adapters/firebase';
 
-export default Ember.Object.extend({
-  store: Ember.inject.service(),
-  open(authentication){
-    return {
-      provider: authentication.provider,
-      currentUser: authentication[authentication.provider]
-    };
-  },
-  fetch() {
-    let store = this.get('store');
-    let adapter = store.adapterFor('application');
-    let firebase = adapter.get('firebase');
-    return new Ember.RSVP.Promise((resolve, reject)=>{
-      let auth = firebase.getAuth();
-      if (auth == null) {
-        reject("No session available");
-      } else {
-        resolve(this.open(auth));
-      }
-    }, "Firebase Torii Adapter#fetch Firebase session");
-  }
+export default ToriiFirebaseAdapter.extend({
+  firebase: Ember.inject.service()
 });
