89a2242d8c1a521169017344e0987faf159f7abd
Taras Mankovski <tarasm@gmail.com>
2015-07-27 07:58:05 -0400

    Updated DSL

--- app/router.js
+++ app/router.js
@@ -1,12 +1,11 @@
 import Ember from 'ember';
 import config from './config/environment';
-import authenticatedRoute from 'torii/routing';
 
 var Router = Ember.Router.extend({
   location: config.locationType
 });
 
 export default Router.map(function() {
-  authenticatedRoute(this, 'add');
-  authenticatedRoute(this, 'users');
+  this.authenticatedRoute('add');
+  this.authenticatedRoute('users');
 });
