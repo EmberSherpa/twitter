7cf385e826b41afaed53fdc2603543e09a705463
Taras Mankovski <tarasm@gmail.com>
2015-08-04 08:50:27 -0400

    Switched to using authenticatedRoute for edit

--- app/router.js
+++ app/router.js
@@ -8,5 +8,5 @@ var Router = Ember.Router.extend({
 export default Router.map(function() {
   this.authenticatedRoute('add');
   this.authenticatedRoute('users');
-  this.route('edit', { path: '/:tweet_id/edit' });
+  this.authenticatedRoute('edit', { path: '/:tweet_id/edit' });
 });
